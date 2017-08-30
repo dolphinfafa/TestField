9c7a67eb54ea179c8343b9072abce16c175fa66e
zhe <leafleaving@gmail.com>
2017-08-30 15:07:37 +0100

    jingpai v0.01

--- website/testfield/testfield/settings.py
+++ website/testfield/testfield/settings.py
@@ -26,6 +26,30 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Template path
+
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+)
+
+# Media path
+
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+
+STATIC_URL = '/static/' # You may find this is already defined as such.
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
+
 
 # Application definition
 
--- website/testfield/testfield/urls.py
+++ website/testfield/testfield/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.conf import settings
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'testfield.views.home', name='home'),
@@ -9,3 +11,10 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^jingpai/', include('jingpai.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
