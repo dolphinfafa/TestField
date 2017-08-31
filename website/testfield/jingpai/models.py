11e138bc5804b14de829477de29fed079a503947
zhe <leafleaving@gmail.com>
2017-08-31 12:40:39 +0100

    jingpai events models

--- website/testfield/jingpai/models.py
+++ website/testfield/jingpai/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Event(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    event_date = models.DateField(null=True)
+
+    event_des = models.TextField()
+
+    def __unicode__(self):  #For Python 2, use __str__ on Python 3
+        return self.name
+
 # Create your models here.
--- website/testfield/jingpai/views.py
+++ website/testfield/jingpai/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+from jingpai.models import Event
+
 def index(request):
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    event_list = Event.objects.order_by('-event_date')
+
+    print event_list
+
+    context_dict= {'events': event_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
